7a9657b3e27dbb735bdc3a2e854576dcf15fffc5
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-01-16 15:54:44 +0700

    Thach 16.1 4:00PM Sửa hiển thị giá tiền và tiền giảm giá

--- swiftshop_application/lib/view_models/searching_screen_view_model.dart
+++ swiftshop_application/lib/view_models/searching_screen_view_model.dart
@@ -18,12 +18,46 @@ class SearchScreenViewModel {
     }
   }
 
-  List lsttype = ["All"];
-
+  List lsttype = [
+    "All",
+    "Giá từ thấp đến cao",
+    "Giá từ cao đến thấp",
+    "Đang khuyến mãi"
+  ];
+  //Thach Sửa lại filter
   void onTapFilter(String type) {
-    type == "All"
-        ? resultProducts = Product.product
-        : resultProducts =
+    // type == "All"
+    //     ? resultProducts = Product.product
+    //     : resultProducts =
+    //         Product.product.where((item) => item.type == type).toList();
+
+    switch (type) {
+      case "All":
+        resultProducts = Product.product;
+        break;
+      case "Giá từ thấp đến cao":
+        resultProducts = Product.product;
+        resultProducts.sort(
+          (a, b) => a.price.compareTo(b.price),
+        );
+        break;
+      case "Giá từ cao đến thấp":
+        resultProducts = Product.product;
+        resultProducts.sort(
+          (a, b) => b.price.compareTo(a.price),
+        );
+        break;
+      case "Đang khuyến mãi":
+        resultProducts = Product.product
+            .where((element) => element.promotionalPrice != 0)
+            .toList();
+        resultProducts.sort(
+          (a, b) => a.promotionalPrice.compareTo(b.promotionalPrice),
+        );
+        break;
+      default:
+        resultProducts =
             Product.product.where((item) => item.type == type).toList();
+    }
   }
 }
